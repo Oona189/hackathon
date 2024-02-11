4c4a8fed5a7b6d6ca23df2665acc9d5a98b1f2ac
BanhmaneMaria <bn25maria@gmail.com>
2024-02-11 14:03:50 +0100

    ajout formulaire partenaire

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import  "./Pages/Ajoutformation/ajoutformation.css";
 import Ajout_thèmes from "./Pages/AjoutThème/Ajout_thèmes";
+import Ajout_partenaire from "./Pages/AjoutPartenaire/Ajout_partenaire";
 function App() {
 
   return (
     <div className='app'>
-    <Ajout_thèmes/>
+    <Ajout_partenaire/>
     </div>
   )
 }
