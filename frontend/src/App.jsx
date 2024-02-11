295d9e9330990801da0817d2d48a850769ff40f3
RAHMA <kr_abdelhamid@esi.dz>
2024-02-11 21:01:08 +0100

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import  "./Pages/Ajoutparticipant/ajoutparticipant.css";
 import Formparticipant from "./Pages/Ajoutparticipant/Formparticipant.jsx";
 import Navbar from "./Navbar/Navbar.jsx";
+import Formations from './Pages/Formations';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     
-    <><div className="container">
-      <Navbar/>
-    <h2 className="title">Ajouter un participant </h2> 
-    <Formparticipant/>
-  </div></>
+    <>
+    <Router>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Formations/>}/>
+        <Route path="/form" element={<Formparticipant/>}/>
+      </Routes>
+    </Router>
+    </>
     
   );
 }export default App
