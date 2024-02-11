4ce5a9b6cabeecaea490faae2152faea6c5cef51
houdahi <hioulhouda@gmail.com>
2024-02-11 19:51:33 +0100

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,14 @@
 import React from "react";
 import  "./Pages/Ajoutparticipant/ajoutparticipant.css";
 import Formparticipant from "./Pages/Ajoutparticipant/Formparticipant.jsx";
+import Navbar from "./Navbar/Navbar.jsx";
+
 
 function App() {
   return (
+    
     <><div className="container">
+      <Navbar/>
     <h2 className="title">Ajouter un participant </h2> 
     <Formparticipant/>
   </div></>
